2ff8426e4222585b479db10b4b633589bface9f0
Dawn Carroll <dsc254@gmail.com>
2018-01-14 17:16:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var name = "Susan"
+<<<<<<< HEAD
 var height = 70
+=======
+var height = "74"
+>>>>>>> c2c0a1619244eb3154ea27d2e91f484fcb273261
+
+if (name === "Susan") {
+  console.log("The name is correct.")
+} else {
+  console.log("Expected the name to be Susan.")
+}
 
 
 // Don't worry about this
